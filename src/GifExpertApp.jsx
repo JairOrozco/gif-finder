6e938ee63b418277b5a8474b3c840c6545d5e593
Jair Orozco <dg.jairsanmartin@gmail.com>
2022-11-29 11:56:33 -0600

    GifExpertApp creando lista de categorias

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
+
 
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['Dragon Ball', 'One Punch'])
+
+    console.log(categories)
+
     return (
         <>
+            {/* Titulo de la app */}
             <h1>GifExpertApp</h1>
+
+            {/* Buscador de gifs */}
+
+
+            {/* Listado de gifs encontrados */}
+            <ol>
+                { categories.map(categoryItem => {
+                    return <li key={ categoryItem }> {categoryItem} </li>
+                }) }
+            </ol>
+                {/* Gif item */}
         </>
     )
 } 

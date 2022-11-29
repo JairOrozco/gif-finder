3165f78f5f800577f429d95e599a17f42cf56bdb
Jair Orozco <dg.jairsanmartin@gmail.com>
2022-11-29 17:48:43 -0600

    GifExpertApp, creamos la función para poder traer los datos de la API de la cual obtenemos id, title y url. Creamos la carpeta utils donde se almacena la función y la importamos desde el componente GifGrid.

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,8 +1,9 @@
-
+import { getDataGifs } from "../utils/getDataGifs";
 
 export const GifGrid = ( { category } ) => {
 
-    
+    getDataGifs(category);
+
     return (
         <>
             <h3> {category} </h3>
---
+++ src/utils/getDataGifs.js
@@ -0,0 +1,21 @@
+export const getDataGifs = async (category) => {
+
+    const url_base = 'https://api.giphy.com/v1/gifs/search'
+    const api_key = 'Sj6EWsy4RV22aZqZngX6De6A6y94ic3s'
+
+    let url_search = `${url_base}?api_key=${api_key}&q=${category}&limit=20`
+
+    const response = await fetch(url_search);
+    const { data } = await response.json();
+
+    const gifs = data.map( img => 
+        ({
+            id: img.id,
+            titlte: img.title,
+            url: img.images.downsized_medium.url
+        }))
+    
+
+    console.log(gifs)
+    return gifs;
+}
